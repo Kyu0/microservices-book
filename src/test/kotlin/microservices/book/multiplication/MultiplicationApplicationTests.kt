732626ddf3966a0bc7c8ac29833088140a8a02ab
Kyu0 <efftar@naver.com>
2023-02-08 12:55:02 +0900

    [fix] 테스트 작성 방식 변경 사유 : @Autowired 적용으로 인해 expression 절 -> body 로 테스트 코드 위치 변경

--- src/test/kotlin/microservices/book/multiplication/MultiplicationApplicationTests.kt
+++ src/test/kotlin/microservices/book/multiplication/MultiplicationApplicationTests.kt
@@ -10,29 +10,32 @@ import microservices.book.multiplication.service.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-class RandomMultiplicationTest: BehaviorSpec({
+class RandomMultiplicationTest: BehaviorSpec() {
     val randomGeneratorService:RandomGeneratorService = mockk<RandomGeneratorService>()
+    @Autowired
     val multiplicationService:MultiplicationService = MultiplicationServiceImpl(randomGeneratorService)
 
-    given("generatorRandomFactor() 메소드가 순차적으로 50, 30을 반환") {
-        every { randomGeneratorService.generateRandomFactor() } returnsMany listOf(50, 30)
-        val multiplication: Multiplication = multiplicationService.createRandomMultiplication()
-        `when`("factorA는") {
-            then("50이다.") {
-                multiplication.getFactorA() shouldBe 50
+    fun `예제 테스트`() {
+        given("generatorRandomFactor() 메소드가 순차적으로 50, 30을 반환") {
+            every { randomGeneratorService.generateRandomFactor() } returnsMany listOf(50, 30)
+            val multiplication: Multiplication = multiplicationService.createRandomMultiplication()
+            `when`("factorA는") {
+                then("50이다.") {
+                    multiplication.getFactorA() shouldBe 50
+                }
             }
-        }
 
-        `when`("factorB는") {
-            then("30이다.") {
-                multiplication.getFactorB() shouldBe 30
+            `when`("factorB는") {
+                then("30이다.") {
+                    multiplication.getFactorB() shouldBe 30
+                }
             }
-        }
 
-        `when`("factorA * factorB는") {
-            then("1500이다.") {
-                multiplication.getResult() shouldBe 1500
+            `when`("factorA * factorB는") {
+                then("1500이다.") {
+                    multiplication.getResult() shouldBe 1500
+                }
             }
         }
     }
-})
+}
