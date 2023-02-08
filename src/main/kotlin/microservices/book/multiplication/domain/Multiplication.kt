acb3b2f6080485db2fe2acb558deeac7ffa4fd21
Kyu0 <efftar@naver.com>
2023-02-08 13:18:35 +0900

    [fix] 필드 접근 방식 변경

--- src/main/kotlin/microservices/book/multiplication/domain/Multiplication.kt
+++ src/main/kotlin/microservices/book/multiplication/domain/Multiplication.kt
@@ -1,22 +1,11 @@
 package microservices.book.multiplication.domain
 
 class Multiplication(
-    private val factorA: Int
-    , private val factorB: Int
+    val factorA: Int
+    , val factorB: Int
 ) {
-    private val result: Int = this.factorA * this.factorB
+    val result: Int = this.factorA * this.factorB
 
-    fun getFactorA():Int {
-        return this.factorA
-    }
-
-    fun getFactorB():Int {
-        return this.factorB
-    }
-
-    fun getResult():Int {
-        return this.result
-    }
 
     override fun toString(): String {
         return "Multiplication{" +
