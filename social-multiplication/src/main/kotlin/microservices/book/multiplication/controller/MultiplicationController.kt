ebc95dd9aa2be18759bafb77f4575bf3df16881f
Kyu0 <efftar@naver.com>
2023-03-18 23:44:32 +0900

    [fix] 로그 추가

--- social-multiplication/src/main/kotlin/microservices/book/multiplication/controller/MultiplicationController.kt
+++ social-multiplication/src/main/kotlin/microservices/book/multiplication/controller/MultiplicationController.kt
@@ -2,17 +2,22 @@ package microservices.book.multiplication.controller
 
 import microservices.book.multiplication.domain.Multiplication
 import microservices.book.multiplication.service.MultiplicationService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MultiplicationController (
-    private val multiplicationService: MultiplicationService
+    private val multiplicationService: MultiplicationService,
+    @Value("\${server.port}")
+    private val serverPort: Int
 ) {
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping("/multiplications/random")
     fun getRandomMultiplication(): Multiplication {
+        log.info("무작위 곱셈이 생성된 서버 @{}", serverPort)
         return multiplicationService.createRandomMultiplication()
     }
 }
